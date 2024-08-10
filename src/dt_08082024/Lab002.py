e3a9ae147c212433d24ceb6ef7ab4b3d9eea1255
balakrishna <balawas678@gmail.com>
2024-08-10 13:58:59 +0530

    changes added

--- src/dt_08082024/Lab002.py
+++ src/dt_08082024/Lab002.py
@@ -1,1 +1,30 @@
-print("Hello")
+# Builtins.py  all the important already built by python people
+# these are like utilities
+# indentation is Imp in Python
+
+#print()
+# self- concept in OOPS which points to itself
+# *args is unlimited number of arguments * -string, int, float, Boolean..
+# sep=' ' - How you want to separate the Arguments
+# end=' /n' - in the end what you want to do
+print("hello", 2, 45, 45.6, "apple", "ram")  # in output all are separated by space
+
+#output is hello 2 45 45.6 apple ram
+
+print("hello", 2, 45, 45.6, "apple", "ram", True, sep="_")
+
+#output is hello_2_45_45.6_apple_ram_True
+
+print("hello", 2, 45, 45.6, "apple", "ram")
+print(True)
+
+# output is hello 2 45 45.6 apple ram
+#           True
+
+print("hello", 2, 45, 45.6, "apple", "ram", end='__')
+print(True)
+
+#output is hello 2 45 45.6 apple ram__True instead of next line True came in same line separated by __
+
+
+
