1821ad393ab2ec04391b906b83f7248b71924619
balakrishna <balawas678@gmail.com>
2024-08-10 15:20:43 +0530

    changes added

---
+++ src/dt_10082024/Lab005.py
@@ -0,0 +1,36 @@
+age = 30
+
+# age -variable-Identifier
+# 123 = 34 is not possible
+# They start with a letter A-Z, a-z or an underscore(_) followed by zero or more letters,underscores, and digits(0-9)
+# python is case sensitive so myvariable and Myvariable are 2 different Identifiers.
+a=10
+_=40
+_=_+2
+
+print(_)
+
+abc123=10
+#123abc=20--not valid
+
+_a="raj"
+_abc="23"
+
+#All the below statements are not possible
+#$a=10
+#print($a)
+#&ba=20
+#print(&ba)
+
+pi=3.14
+name="vish"
+iscorrect= True
+
+print(type(pi))
+print(type(name))
+print(type(iscorrect))
+
+complex=2+3j
+
+print(complex.real)
+print(complex.imag)
