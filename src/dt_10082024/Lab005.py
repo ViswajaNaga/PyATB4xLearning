d4018c8c93fb3e64e321cd42f9fcf00e3a2d2e55
balakrishna <balawas678@gmail.com>
2024-08-13 14:26:49 +0530

    changes added

--- src/dt_10082024/Lab005.py
+++ src/dt_10082024/Lab005.py
@@ -1,36 +1,36 @@
 age = 30
-
-# age -variable-Identifier
+# age -variable name-Identifier
+# 30 - Variable Value or Literal
 # 123 = 34 is not possible
 # They start with a letter A-Z, a-z or an underscore(_) followed by zero or more letters,underscores, and digits(0-9)
 # python is case sensitive so myvariable and Myvariable are 2 different Identifiers.
-a=10
-_=40
-_=_+2
+a = 10
+_ = 40
+_ = _ + 2
 
 print(_)
 
-abc123=10
-#123abc=20--not valid
+abc123 = 10
+# 123abc=20--not valid
 
-_a="raj"
-_abc="23"
+_a = "raj"
+_abc = "23"
 
-#All the below statements are not possible
-#$a=10
-#print($a)
-#&ba=20
-#print(&ba)
+# All the below statements are not possible
+# $a=10
+# print($a)
+# &ba=20
+# print(&ba)
 
-pi=3.14
-name="vish"
-iscorrect= True
+pi = 3.14
+name = "vish"
+is_correct = True
 
 print(type(pi))
 print(type(name))
-print(type(iscorrect))
+print(type(is_correct))
 
-complex=2+3j
+complex = 2 + 3j
 
 print(complex.real)
 print(complex.imag)
