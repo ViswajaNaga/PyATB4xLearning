72bd5edba8a5d6b661b833235f5af0fd9f1914c5
balakrishna <balawas678@gmail.com>
2024-08-13 14:19:30 +0530

    changes added

--- src/dt_10082024/Lab006.py
+++ src/dt_10082024/Lab006.py
@@ -1,16 +1,13 @@
-#Python is Dynamically Typed Language
-a=10
-b=10
-c=10
-sum=a+b+c
-sum=sum-10
-sum=sum+5
-sum+2  # this is not evaluated as it is not assigned to a variable sum meaning not stored
+# Python is Dynamically Typed Language
+a = 10
+b = 10
+c = 10
+sum = a + b + c
+sum = sum - 10
+sum = sum + 5
+#sum + 2  # this is not evaluated as it is not assigned to a variable sum meaning not stored
 print(sum)
 
-a,b,c=45,54,89
-print(a,b,c)
-print(a,b,c,sep='|')
-
-
-
+a, b, c = 45, 54, 89
+print(a, b, c)
+print(a, b, c, sep='|')
