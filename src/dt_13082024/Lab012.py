e4b94e3f171058cd11e7002f142e2d755cf3731c
balakrishna <balawas678@gmail.com>
2024-08-13 19:10:37 +0530

    new learnings

---
+++ src/dt_13082024/Lab012.py
@@ -0,0 +1,19 @@
+# Take 2 inputs and perform operations
+num1=int(input("enter num1 "))
+num2=int(input("enter num2 "))
+
+print(max(num1,num2))
+sum=num1+num2
+print(sum)
+
+mul=num1*num2
+print(mul)
+
+div=num1/num2
+print(div)
+
+sub=num1-num2
+print(sub)
+
+power=pow(num1,num2)
+print(power)
