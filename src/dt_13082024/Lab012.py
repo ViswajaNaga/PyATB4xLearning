4b390490df7691a82440d22c8296095fcbe9bfd7
balakrishna <balawas678@gmail.com>
2024-08-13 19:36:07 +0530

    updated

--- src/dt_13082024/Lab012.py
+++ src/dt_13082024/Lab012.py
@@ -2,18 +2,21 @@
 num1=int(input("enter num1 "))
 num2=int(input("enter num2 "))
 
-print(max(num1,num2))
-sum=num1+num2
-print(sum)
+print("max of two numbers is", max(num1,num2))
+print(f"max of {num1} and {num2} is {max(num1,num2)}")
 
-mul=num1*num2
-print(mul)
+print("sum of two numbers is", num1+num2)
+print(f"sum of {num1} and {num2} is {num1+num2}")
 
-div=num1/num2
-print(div)
+print("multiplication of two numbers is", num1*num2)
+print(f"Multiplication of {num1} and {num2} is {num1*num2}")
 
-sub=num1-num2
-print(sub)
+print("Division of two numbers is", num1/num2)
+print(f"Division of {num1} and {num2} is {num1/num2}")
 
-power=pow(num1,num2)
-print(power)
+print("Subtraction of {num1} and {num2} is", num1-num2)
+print(f"Subtraction of {num1} and {num2} is", (num1-num2))
+
+print("power of num1 to num2 is", pow(num1,num2))
+print(f"power of {num1} to {num2} is", pow(num1,num2))
+
